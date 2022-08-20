895cb23f27e1ff0afdfff63f87e83c5dcf0fdcfe
njmertz <nick.mertz@gmail.com>
2022-08-20 17:13:44 -0500

    Cleaned up formatting in the files

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -12,15 +12,11 @@ const getWorkouts = async (req, res) => {
 const getWorkout = async (req, res) => {
   const {id} = req.params;
 
-  if(!mongoose.Types.ObjectId.isValid(id)){
-    return res.status(404).json({error: "No such workout"});
-  }
+  if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({error: "No such workout"});
 
   const workout = await Workout.findById(id);
 
-  if(!workout){
-     return res.status(404).json({error: 'No such workout'});
-  }
+  if(!workout) return res.status(404).json({error: 'No such workout'});
 
   res.status(200).json(workout);
 };
@@ -31,21 +27,13 @@ const createWorkout = async (req, res) => {
 
   let emptyFields = [];
 
-  if(!title){
-    emptyFields.push('title');
-  }
+  if(!title) emptyFields.push('title');
 
-  if(!load){
-    emptyFields.push('load');
-  }
+  if(!load) emptyFields.push('load');
   
-  if(!reps){
-    emptyFields.push('reps');
-  }
+  if(!reps) emptyFields.push('reps');
   
-  if(emptyFields.length > 0){
-    return res.status(400).json({error: 'Please fill in all the fields', emptyFields});
-  }
+  if(emptyFields.length > 0) return res.status(400).json({error: 'Please fill in all the fields', emptyFields});
 
   try {
     const workout = await Workout.create({
@@ -61,15 +49,11 @@ const createWorkout = async (req, res) => {
 const deleteWorkout = async (req, res) => {
   const { id } = req.params;
 
-  if(!mongoose.Types.ObjectId.isValid(id)){
-    return res.status(404).json({error: 'No such workout'});
-  }
+  if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({error: 'No such workout'});
 
   const workout = await Workout.findByIdAndDelete({_id: id});
 
-  if(!workout){
-    return res.status(404).json({error: 'No such workout'});
-  }
+  if(!workout) return res.status(404).json({error: 'No such workout'});
 
   res.status(200).json(workout);
 };
@@ -78,17 +62,13 @@ const deleteWorkout = async (req, res) => {
 const updateWorkout = async (req, res) => {
   const { id } = req.params;
 
-  if(!mongoose.Types.ObjectId.isValid(id)){
-    return res.status(404).json({error: 'No such workout'});
-  }
+  if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({error: 'No such workout'});
   
   const workout = await Workout.findOneAndUpdate({_id: id}, {
     ...req.body
   });
   
-  if(!workout){
-    return res.status(404).json({error: 'No such workout'});
-  }
+  if(!workout) return res.status(404).json({error: 'No such workout'});
 
   res.status(200).json(workout);
 }
--- backend/server.js
+++ backend/server.js
@@ -23,13 +23,11 @@ app.use((req, res, next) => {
 app.use('/api/workouts', workoutRoutes);
 
 // Connect to database
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    // Listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log('connected to database and listening on port', process.env.PORT);
-    });
-  })
-  .catch((error) => {
-    console.log(error);
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  // Listen for requests
+  app.listen(process.env.PORT, () => {
+    console.log('Connected to database and listening on port', process.env.PORT);
   });
+}).catch((error) => {
+  console.log(error);
+});

6b1a0214a0a394f925b7d6357876a02d067bb42e
njmertz <nick.mertz@gmail.com>
2022-08-20 10:38:42 -0500

    Initial commit before auth implementation.

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts');
+
+// Express app
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// Routes
+app.use('/api/workouts', workoutRoutes);
+
+// Connect to database
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // Listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to database and listening on port', process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
